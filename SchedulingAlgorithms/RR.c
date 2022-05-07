af207923658cf70b1b68b633b3e45fe23ac8cf7d
Mohamed Kamal <medoking91@gmail.com>
2022-05-07 16:54:55 +0200

    RR working 98% accuracy

--- SchedulingAlgorithms/RR.c
+++ SchedulingAlgorithms/RR.c
@@ -72,19 +72,19 @@ void RRNewProcessHandler(void *ReadyQueue, process *p)
 
 void RRNewProcessFinalizationHandler(void *ReadyQueue)
 {
-    //printf("Starting A Process\n");
-    
-    if(CurrentProcess != NULL)
+    // printf("Starting A Process\n");
+
+    if (CurrentProcess != NULL)
     {
         return;
     }
     process *p = RRRunNext(ReadyQueue);
     curr_quantum = 0;
-    if(p->state == STOPPED)
+    if (p->state == STOPPED)
     {
-        kill(p->pWaitId,SIGCONT);
+        kill(p->pWaitId, SIGCONT);
         p->state = RESUMED;
-        printf("Process %d Resumed! remain time %d\n", p ->pid ,*p->remainingTime);
+        printf("Process %d Resumed! remain time %d\n", p->pid, *p->remainingTime);
         logProcess(logFile, p, time_after);
         CurrentProcess = p;
     }
@@ -97,12 +97,13 @@ void RRNewProcessFinalizationHandler(void *ReadyQueue)
         logProcess(logFile, p, time_after);
     }
 
-    //printf("Started A Process\n");
+    // printf("Started A Process\n");
 }
 
 void RREnqueue(void *ReadyQueue, process *p)
 {
     enqueueCircularQueue((CircularQueue *)ReadyQueue, p);
+    printf("######enqueued process id = %d\n", p->pid);
 }
 
 process *RRRunNext(void *ReadyQueue)
@@ -110,6 +111,7 @@ process *RRRunNext(void *ReadyQueue)
     if (isCircularQueueEmpty((CircularQueue *)ReadyQueue))
         return NULL;
     process *p = dequeueCircularQueue((CircularQueue *)ReadyQueue);
+    printf("######dequeued process id = %d\n", p->pid);
     return p;
 }
 
@@ -118,28 +120,37 @@ void SwitchProcess(void *ReadyQueue)
 
     if (isCircularQueueEmpty((CircularQueue *)ReadyQueue) == false)
     {
-        if(CurrentProcess != NULL)
+        if (CurrentProcess != NULL)
         {
+            if (*CurrentProcess->remainingTime > 0)
+                RREnqueue(ReadyQueue, CurrentProcess);
+            else
+            {
+                CurrentProcess->state = FINISHED;
+                // logProcess(logFile, CurrentProcess, time_after);
+                return;
+            }
+
             printf("Process %d Switched!\n", CurrentProcess->pid);
             // Stop Current Process
             kill((pid_t)CurrentProcess->pWaitId, SIGSTOP);
             CurrentProcess->state = STOPPED;
             logProcess(logFile, CurrentProcess, time_after);
-            RREnqueue(ReadyQueue, CurrentProcess);
+            printf("Process %d Stopped!remain time %d\n", CurrentProcess->pid, *CurrentProcess->remainingTime);
         }
 
-        // // Start Next One
+        // Start Next One
         // process *p = RRRunNext(ReadyQueue);
         // create_process(p);
-        // //p->waitTime = p->waitTime == 0 ? time_after - p->arrivalTime : p->waitTime;
+        // p->waitTime = p->waitTime == 0 ? time_after - p->arrivalTime : p->waitTime;
         // CurrentProcess = p;
         // logProcess(logFile, p, time_after);
         process *p = RRRunNext(ReadyQueue);
-        if(p->state == STOPPED)
+        if (p->state == STOPPED)
         {
-            kill(p->pWaitId,SIGCONT);
+            kill(p->pWaitId, SIGCONT);
             p->state = RESUMED;
-            printf("Process %d Resumed! remain time %d\n", p ->pid ,*p->remainingTime);
+            printf("Process %d Resumed! remain time %d\n", p->pid, *p->remainingTime);
             logProcess(logFile, p, time_after);
             CurrentProcess = p;
         }
@@ -154,8 +165,8 @@ void SwitchProcess(void *ReadyQueue)
     }
     else
     {
-        //curr_quantum = quantum_size - 1;
-        //curr_quantum = 0;
+        // curr_quantum = quantum_size - 1;
+        // curr_quantum = 0;
     }
 }
 
@@ -164,7 +175,7 @@ void RRTimeSlotHandler(void *ReadyQueue)
     if (CurrentProcess == NULL)
     {
         curr_quantum = 0;
-        SwitchProcess(ReadyQueue);   
+        SwitchProcess(ReadyQueue);
         return;
     }
     curr_quantum++;
@@ -185,7 +196,6 @@ void RRTerminationHandler(void *ReadyQueue)
     CurrentProcess = NULL;
     curr_quantum = 0;
     SwitchProcess(ReadyQueue);
-
 }
 
 void RRDestroy(void *ReadyQueue)
